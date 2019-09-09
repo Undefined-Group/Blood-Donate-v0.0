7fcdff0a01c2884252da99711a75ab0485bbdb07
Rawabi Okour <rawabi.f.okour@gmail.com>
2019-09-09 17:01:59 +0300

    version_1

--- React/src/App.js
+++ React/src/App.js
@@ -1,23 +1,44 @@
 import React from 'react';
-// import { BrowserRouter as Router, Swich, Route } from 'react-router-dom'
-// import axios from './components/Axios';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import axios from './components/Axios';
+// import axios from 'axios'
+import AddDonor from './components/AddDonor'
+import Home from './components/Home'
 
 
 class App extends React.Component {
   state = {
-    search: []
+    search: [],
+    DonorData:[]
   }
+
+// Add Donor
+  postDonor = (firstName, lastName, phone ,country,city,bloodType,email,birthday,cleanInput) => {
+    console.log("Post Donor")
+    axios
+      .post(`/${firstName}/${lastName}/${phone}/${country}/${city}/${bloodType}/${email}/${birthday}`)
+      .then(response => {
+        this.setState({ DonorData: response.data });
+        
+      });
+      console.log("data",this.state.DonorData)
+      cleanInput()  
+  };
+
 
   render() {
     return (
       <>
-        {/* <Router>
+         <Router>
+         <Route path="/" exact component={Home} /> 
+          <Route path="/adddonor" component={()=><AddDonor postDonor={this.postDonor}/>} /> 
+          
+        
 
 
 
 
-
-        </Router> */}
+        </Router> 
       </>
     );
   }
