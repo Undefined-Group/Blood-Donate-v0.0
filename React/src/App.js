634c18a9b247e89002a2e0662cecad0f8eb18742
dohadojan <dojandoha@gmail.comgit>
2019-09-09 16:44:09 +0300

    “SearchComponent”

--- React/src/App.js
+++ React/src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Search from './components/Search'
+
+
+
 // import { BrowserRouter as Router, Swich, Route } from 'react-router-dom'
 // import axios from './components/Axios';
 
 
 class App extends React.Component {
   state = {
-    search: []
+    Search: []
   }
 
+
   render() {
+
+
+
     return (
+
+      
       <>
-        {/* <Router>
+        {/* <Router> */}
+<Search> 
+
+
+  
+</Search>
+
+       
+        {/* ); */}
 
 
 
 
+        {/* </Router> */}
 
-        </Router> */}
       </>
     );
   }
 }
+
+
+
+
+
+
+
 
 export default App;
